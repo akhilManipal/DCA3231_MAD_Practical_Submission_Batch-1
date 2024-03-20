6bde5bff884f7056c3d265f7391ddfd4242d52c1
Akhil <akhil.menon@hurix.com>
2024-03-20 14:46:19 +0530

    first push of code

---
+++ Practical 1_Temperature Conversion/app/src/main/java/com/example/tempconversion/ConverterUtil.java
@@ -0,0 +1,8 @@
+package com.example.tempconversion;
+
+public class ConverterUtil {
+    // Converts to Fahrenheit
+    public static double convertCelsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5.0) + 32;
+    }
+}
